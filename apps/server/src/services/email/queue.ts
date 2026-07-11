9c8d7e2bc4d5b2596b31ff38842e9fe6d0ba7729
3qrain <1360575872@qq.com>
2026-07-11 21:17:58 +0800

    chore: 后端邮件发送并发量大一点，邮件发送有时有点太慢了，串行发太慢

--- apps/server/src/services/email/queue.ts
+++ apps/server/src/services/email/queue.ts
@@ -3,7 +3,7 @@ type Task = () => Promise<void>
 const queue: Task[] = []
 let running = 0
 // 邮件发送并发量
-const MAX_CONCURRENCY = 1
+const MAX_CONCURRENCY = 3
 
 function next() {
   while (running < MAX_CONCURRENCY && queue.length > 0) {
@@ -20,6 +20,7 @@ function next() {
 }
 
 export function enqueue(task: Task) {
+  // if (queue.length > 999) throw new Error('邮件发送队列已满')
   queue.push(task)
   next()
 }
